8213e1b9366fc5c829a3f77227d062abc3cf6f8a
Katarzyna Chachlowska <chachlowskak@gmail.com>
2017-06-03 11:02:06 +0200

    Changed database adapter to psql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170603082540) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "programs", force: :cascade do |t|
     t.integer "number"
     t.string "name"
