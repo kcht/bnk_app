7dcba9c4d77ebce4bbda907adaf0f6fe801cbd6d
Katarzyna Chachlowska <chachlowskak@gmail.com>
2017-06-20 23:18:51 +0200

    Basic styles added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'static_page#home'
 
-  get 'static_page/home'
-  get 'static_page/help'
-  get 'static_page/about'
+  get 'home', to: 'static_page#home'
+  get 'help', to: 'static_page#help'
+  get 'about', to: 'static_page#about'
+  get 'contact', to: 'static_page#contact'
+  get  'signup',  to: 'users#new'
 
   resources :users
 
--- spec/controllers/static_page_controller_spec.rb
+++ spec/controllers/static_page_controller_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe StaticPageController, type: :controller do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
-      assert_select 'title', "Home | #{base_title}"
     end
   end
 
