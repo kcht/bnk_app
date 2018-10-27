7e92339f407a62545de24842f7aad184a8a2ae34
Katarzyna Chachlowska <chachlowskak@gmail.com>
2018-10-27 12:16:29 +0200

    Move playlist formatting logic from rake task to a service

---
+++ app/services/playlists/format_from_file.rb
@@ -0,0 +1,60 @@
+require 'dry/monads/all'
+
+module Playlists
+  class FormatFromFile
+    include Dry::Monads::Result::Mixin
+
+    BNK_DATA_PATH = "db/bnk_data"
+
+    def initialize(program_number)
+      @program_number = program_number
+    end
+
+    def call
+      Success(program_number: program_number)
+          .bind(method(:check_file_exists))
+          .bind(method(:readlines))
+          .bind(method(:discard_non_playlist_lines))
+          .bind(method(:format_playlist_output))
+    end
+
+    private
+
+    attr_reader :program_number
+
+
+    def check_file_exists(program_number:)
+      regex = "bnk_#{program_number.to_s.rjust(3,'0')}"
+      entries = Dir.entries(BNK_DATA_PATH).select {|f| f.match(/#{Regexp.quote(regex)}/)}
+      entry = entries.first
+      entry.present? ? Success(entry: entry) : Failure('file doesnt exist')
+    end
+
+    def readlines(entry:)
+      lines = File.readlines("#{BNK_DATA_PATH}/#{entry}")
+      Success(lines: lines)
+    end
+
+    def discard_non_playlist_lines(lines:)
+      lines.select! {|line| line =~ /^[0-9]/}
+      Success(lines: lines)
+    end
+
+    def format_playlist_output(lines:)
+      output = []
+      lines.each do |line|
+        number = line.split(':').first
+        song_info = line.split(':').second.strip
+        split = song_info.split(';')
+        title = split[0].strip
+        artist = split[1].strip
+        album = split[2].strip
+        year = split[3].strip
+
+        output << "#{number}. #{title} - #{artist} (#{album}, #{year})"
+      end
+
+      Success(output)
+    end
+  end
+end
--- lib/tasks/playlist.rake
+++ lib/tasks/playlist.rake
@@ -1,38 +1,9 @@
 namespace :playlist do
   @logger = Logger.new(STDOUT)
   desc 'Import BNK data from files to db'
-  task :formatted, [:number] => [:environment] do |task, args|
-    BNK_DATA_PATH = "db/bnk_data"
-
-    regex = "bnk_#{formatted_number(args[:number])}"
-    entries = Dir.entries(BNK_DATA_PATH).select {|f| f.match(/#{Regexp.quote(regex)}/)}
-    entry = entries.first
-
-    lines = File.readlines("#{BNK_DATA_PATH}/#{entry}")
-    lines.select! { |line| line =~ /^[0-9]/ }
-
-    lines.each do |line|
-      number = line.split(':').first
-      rest = line.split(':').second.strip
-      split = rest.split(';')
-      title =  split[0].strip
-      artist = split[1].strip
-      album = split[2].strip
-      year = split[3].strip
-
-      puts "#{number}. #{title} - #{artist} (#{album}, #{year})"
-    end
-  end
-
-  def formatted_number(number)
-    number = number.to_i
-    if number < 10
-      "00#{number}"
-    elsif number < 100
-      "0#{number}"
-    else
-      number
-    end
+  task :formatted, [:number] => [:environment] do |_task, args|
+    result = Playlists::FormatFromFile.new(args[:number]).call.value!
+    puts result.join("\n")
   end
 end
 
